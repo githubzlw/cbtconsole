d63265c2a937a6f66209962b8b484bac8ea23bea
whj <919923437@qq.com>
2018-11-30 17:41:16 +0800

    kara权限修改

--- src/main/java/com/cbt/orderinfo/ctrl/OrderInfoController.java
+++ src/main/java/com/cbt/orderinfo/ctrl/OrderInfoController.java
@@ -605,7 +605,7 @@ public class OrderInfoController{
 		}
 		Admuser user = (Admuser)SerializeUtil.JsonToObj(admJson, Admuser.class);
 		String strm=user.getRoletype(); int admuserid=user.getId();
-		if("0".equals(strm) || user.getAdmName().equalsIgnoreCase("Sales1") || user.getAdmName().equalsIgnoreCase("emmaxie") || user.getAdmName().equalsIgnoreCase("yangyunyun")
+		if("0".equals(strm) || user.getAdmName().equalsIgnoreCase("Sales1") || user.getAdmName().equalsIgnoreCase("emmaxie") || user.getAdmName().equalsIgnoreCase("kara")
 				|| user.getAdmName().equalsIgnoreCase("admin1")){
 			admuserid = Utility.getStringIsNull(admuserid_str) ? Integer.parseInt(admuserid_str) : 0;
 		}
@@ -709,7 +709,7 @@ public class OrderInfoController{
 			String admJson = Redis.hget(request.getSession().getId(), "admuser");
 			Admuser user = (Admuser)SerializeUtil.JsonToObj(admJson, Admuser.class);
 			String strm=user.getRoletype(); int admuserid=user.getId();
-			if("0".equals(strm) || user.getAdmName().equalsIgnoreCase("Sales1") || user.getAdmName().equalsIgnoreCase("yangyunyun")
+			if("0".equals(strm) || user.getAdmName().equalsIgnoreCase("Sales1") || user.getAdmName().equalsIgnoreCase("kara")
 					|| user.getAdmName().equalsIgnoreCase("emmaxie") || user.getAdmName().equalsIgnoreCase("admin1")){
 				//临时添加Sales1查看所有订单列表的统计
 				admuserid = Utility.getStringIsNull(admuserid_str) ? Integer.parseInt(admuserid_str) : 0;
