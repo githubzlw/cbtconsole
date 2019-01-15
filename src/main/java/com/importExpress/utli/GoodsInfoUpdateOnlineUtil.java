c4dbb789c3df354e39adb5edece81c7e76814063
Allen <1071083166@qq.com>
2019-01-15 13:24:25 +0800

    设置封面图和原来默认橱窗图放在本地压缩和上传图片

--- src/main/java/com/importExpress/utli/GoodsInfoUpdateOnlineUtil.java
+++ src/main/java/com/importExpress/utli/GoodsInfoUpdateOnlineUtil.java
@@ -154,7 +154,7 @@ public class GoodsInfoUpdateOnlineUtil {
         inputData.setGoodsstate("4");
         inputData.setFprice_str(bean.getFpriceStr());
         inputData.setSize_info_en(bean.getSizeInfoEn());
-        inputData.setCustom_main_image(bean.getShowMainImage());
+        inputData.setCustom_main_image(bean.getShowMainImage().replace(bean.getRemotpath(),""));
         //最终更新的json数据,json数据现在按照jack要求是写入文件，一条json数据对应一条语句 写在文件的一行，然后文件提供到jack
         return updateLocalAndSolr(inputData, 1);
     }
