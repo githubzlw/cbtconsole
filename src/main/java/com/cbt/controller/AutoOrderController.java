22519ee9330fa556723edeebdc23eac72414b928
caojicao <saycjc@outlook.com>
2019-03-25 17:57:43 +0800

    invoice 获取不到问题

--- src/main/java/com/cbt/controller/AutoOrderController.java
+++ src/main/java/com/cbt/controller/AutoOrderController.java
@@ -292,11 +292,8 @@ public class AutoOrderController {
 	    fileByOrderid = fileByOrderid==null?"":fileByOrderid;
 	    fileByOrderid = fileByOrderid.endsWith(",")?
 	    		fileByOrderid.substring(0,fileByOrderid.length()-1):fileByOrderid;
-	    
+		fileByOrderid = fileByOrderid.replaceAll("F:","/root/F:");
 	    String[] files = fileByOrderid.split(",");
-		Arrays.stream(files).forEach(e->{
-			e="/root/"+e;
-		});
 	    if(Integer.valueOf(index)<files.length){
 	    	fileByOrderid = files[Integer.valueOf(index)];
 	    }
