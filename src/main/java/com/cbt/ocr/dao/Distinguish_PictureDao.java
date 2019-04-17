832cd175f9c1f9c220ee8e367ec2807714fbfc43
zhaolc <1757566620@qq.com>
2019-04-17 17:46:53 +0800

    ocr上线2

--- src/main/java/com/cbt/ocr/dao/Distinguish_PictureDao.java
+++ src/main/java/com/cbt/ocr/dao/Distinguish_PictureDao.java
@@ -1,9 +1,9 @@
 package com.cbt.ocr.dao;
 
+import com.cbt.pojo.Admuser;
 import com.cbt.pojo.Category1688;
 import com.cbt.pojo.CustomGoods;
 import org.apache.ibatis.annotations.Param;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 import java.util.Map;
@@ -13,7 +13,7 @@ public interface Distinguish_PictureDao {
 
 	public List<CustomGoods> showDistinguish_Pircture(@Param("pid")String pid,  @Param("page")int page, @Param("imgtype")String imgtype, @Param("state")String state, @Param("Change_user")String Change_user);
 
-	public List<CustomGoods> showDistinguish_Pircture_2();
+	public List<Admuser> showDistinguish_Pircture_2();
 
 	public int queryDistinguish_PirctureCount(@Param("pid")String pid, @Param("imgtype")String imgtype, @Param("state")String state, @Param("Change_user")String Change_user);
 
