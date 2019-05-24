99b1da1490f0d46da41e6d3bf675b67c51acefcb
Jiangxw <1071083166@qq.com>
2019-05-24 18:06:18 +0800

    1.中文索引查询不了处理 2.采购补货验货不了问题查找

--- src/main/java/com/cbt/controller/CustomGoodsController.java
+++ src/main/java/com/cbt/controller/CustomGoodsController.java
@@ -157,8 +157,8 @@ public class CustomGoodsController {
 
 		CustomGoodsQuery queryBean = genQueryBean(request);
 
-		String chKeyWord = request.getParameter("chKeyWord");
-		/*if(StringUtils.isNotBlank(chKeyWord)){
+		/*String chKeyWord = request.getParameter("chKeyWord");
+		if(StringUtils.isNotBlank(chKeyWord)){
 			try{
 				chKeyWord  = new String(chKeyWord.getBytes("ISO-8859-1"),"UTF-8");
 				queryBean.setChKeyWord(chKeyWord);
