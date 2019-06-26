e7b14eedc0cc13f13ced40fe22d696cd91e35eb9
liuyong <iscati@163.com>
2019-06-26 11:47:24 +0800

    后台 商品下架审核 调整（增加显示同款销量；是否属于核心供应商;）

--- src/main/java/com/cbt/bean/SameGoodsDetails.java
+++ src/main/java/com/cbt/bean/SameGoodsDetails.java
@@ -9,5 +9,7 @@ public class SameGoodsDetails {
 	private String shopId;
 	private String shopName;
 	private String jsonContent;
+	private String bookedCnt;
+	private Boolean salesShop = false;  // 是否是 核心供应商 默认false 不是
 
 }
