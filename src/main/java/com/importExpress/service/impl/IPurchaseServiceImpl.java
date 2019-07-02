2a4d7e379a06378704f407b1426f64596668947a
zql <liaozq0426@163.com>
2019-07-02 09:53:17 +0800

    采购页面增加1688订单号和物流单号

--- src/main/java/com/importExpress/service/impl/IPurchaseServiceImpl.java
+++ src/main/java/com/importExpress/service/impl/IPurchaseServiceImpl.java
@@ -1258,8 +1258,10 @@ public class IPurchaseServiceImpl implements IPurchaseService {
 			Set<String> pid_list=new HashSet<String>();
 			for(int i=0;i<pbList.size();i++){
 				PurchasesBean shipBean=this.customGoodsMapper.FindShipnoByOdid(pbList.get(i));
-				pbList.get(i).setShipnoid(shipBean.getShipnoid());
-				pbList.get(i).setTborderid(shipBean.getTborderid());
+				if (shipBean !=null) {
+					pbList.get(i).setShipnoid(shipBean.getShipnoid());
+					pbList.get(i).setTborderid(shipBean.getTborderid());
+				}
 				pid_list.add(pbList.get(i).getGoods_pid());
 			}
 			double pid_amount=0;
