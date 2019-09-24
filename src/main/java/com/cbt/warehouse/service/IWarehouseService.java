bcc0151f7910dfdfbef673d0e7e1e59fff4e6eaa
Jiangxw <1071083166@qq.com>
2019-09-24 10:05:26 +0800

    出库更新运单信息添加MQ更新日志

--- src/main/java/com/cbt/warehouse/service/IWarehouseService.java
+++ src/main/java/com/cbt/warehouse/service/IWarehouseService.java
@@ -1166,5 +1166,14 @@ public interface IWarehouseService {
     List<Inventory> FindAllGoods(int page,int pagesize,String pid);
 
 	boolean setInventoryCountBySkuAndPid(List<SampleOrderBean> list);
+
+	/**
+	 * 插入MO Sql日志
+	 * @param sqlStr
+	 * @param shopNo
+	 * @param orderNo
+	 * @return
+	 */
+	int insertMqLog(String sqlStr, String shopNo, String orderNo,  String paramStr);
 }
  
