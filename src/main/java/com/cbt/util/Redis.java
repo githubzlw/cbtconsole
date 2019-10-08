85407db8c2572ca98519b89c01cfb89c5a856a6b
jack.luo <luohao518@yeah.net>
2019-10-08 12:16:31 +0800

    连接池参数调整&代码优化

--- src/main/java/com/cbt/util/Redis.java
+++ src/main/java/com/cbt/util/Redis.java
@@ -1,198 +1,118 @@
 package com.cbt.util;
 
-import com.cbt.warehouse.ctrl.HotGoodsCtrl;
 import org.slf4j.LoggerFactory;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 
-import javax.servlet.jsp.tagext.TryCatchFinally;
-
 
 /**
- * 	Redis 连接池
- * @author Administrator
+ * Redis 连接池
  *
+ * @author Administrator
  */
 public class Redis {
 
-	private static JedisPool pool = null;
+    private final static org.slf4j.Logger logger = LoggerFactory.getLogger(Redis.class);
+    private static JedisPool pool = null;
 
-	private final static org.slf4j.Logger logger = LoggerFactory.getLogger(Redis.class);
+    private static Jedis getJedis() {
+        Jedis jedis = null;
+        try {
+            pool = getPool();
+            jedis = pool.getResource();
+            logger.info("get redis connect  active:[{}] idle:[{}] waiter:[{}]",pool.getNumActive(),pool.getNumIdle(),pool.getNumWaiters());
 
-	/**
-	 * 构建redis连接池
-	 * 
-	 * @return JedisPool
-	 */
-	public synchronized static JedisPool getPool() {
-		if (pool == null) {
-			logger.info("redis begin init");
-			String redisIP = SysParamUtil.getParam("RedisIP");
-			String redisPort = SysParamUtil.getParam("RedisPort");
-			logger.info("Redis config:{} {}",redisIP,redisPort);
-			JedisPoolConfig config = new JedisPoolConfig();
-			//最大连接数
-			config.setMaxTotal(Integer.parseInt(SysParamUtil.getParam("MaxTotal")));
-			//最大空闲数
-			config.setMaxIdle(Integer.parseInt(SysParamUtil.getParam("MaxIdle")));
-			//最大等待毫秒数
-			config.setMaxWaitMillis(Long.parseLong(SysParamUtil.getParam("MaxWaitMillis")));
-			// 在连接还回给pool时，是否提前进行validate操作
-			config.setTestOnReturn(true);
-			//如果一个连接300秒内没有任何的返回Jedis将关闭这个连接
-			pool = new JedisPool(config, redisIP, Integer.parseInt(redisPort),Integer.parseInt(SysParamUtil.getParam("PoolTimeout")),SysParamUtil.getParam("RedisAuthPass"));
-			logger.info("redis end init");
-		}
-		return pool;
-	}
-	
-	
-	
-	/**
-     * 返还到连接池
-     * 
-     * @param redis
-     */
-	public static void returnResource(Jedis redis) {
-        if (redis != null) {
-	        logger.info("close redis connect");
-	        redis.close();
+            return jedis;
+        } catch (Exception e) {
+            logger.error("error", e);
+            throw new RuntimeException("redis exception");
         }
     }
 
-	public static Jedis getJedis()  {
-		Jedis jedis = null;
-		try {
-			pool = getPool();
-			jedis = pool.getResource();
-			logger.info("get redis connect");
-			return jedis;
-		}catch(Exception e){
-			logger.error("error",e);
-			throw new RuntimeException("redis exception");
-		}
-	}
+    /**
+     * 构建redis连接池
+     *
+     * @return JedisPool
+     */
+    private synchronized static JedisPool getPool() {
+        if (pool == null) {
+            logger.info("redis pool init begin");
+            String redisIP = SysParamUtil.getParam("RedisIP");
+            String redisPort = SysParamUtil.getParam("RedisPort");
+	        int intMaxTotal=Integer.parseInt(SysParamUtil.getParam("MaxTotal"));
+	        int intMaxIdle=Integer.parseInt(SysParamUtil.getParam("MaxIdle"));
+	        long lngMaxWaitMillis=Long.parseLong(SysParamUtil.getParam("MaxWaitMillis"));
+	        int intPoolTimeout=Integer.parseInt(SysParamUtil.getParam("PoolTimeout"));
+            logger.info("Redis config IP:[{}] PORT:[{}] intMaxTotal:[{}] intMaxIdle:[{}] lngMaxWaitMillis:[{}] intPoolTimeout:[{}]", redisIP, redisPort,intMaxTotal,intMaxIdle,lngMaxWaitMillis,intPoolTimeout);
+            JedisPoolConfig config = new JedisPoolConfig();
+            //最大连接数
+            config.setMaxTotal(intMaxTotal);
+            //最大空闲数
+            config.setMaxIdle(intMaxIdle);
+            config.setMinIdle(intMaxIdle);
+            //最大等待毫秒数
+            config.setMaxWaitMillis(lngMaxWaitMillis);
+            // 在连接还回给pool时，是否提前进行validate操作
+            config.setTestOnReturn(true);
+            //如果一个连接300秒内没有任何的返回Jedis将关闭这个连接
+            pool = new JedisPool(config, redisIP, Integer.parseInt(redisPort), intPoolTimeout, SysParamUtil.getParam("RedisAuthPass"));
+            logger.info("redis pool init end");
+        }
+        return pool;
+    }
+
+    /**
+     * 设置redis哈希内容, 对哈希进行hset不会修改已经设置的key过期时间
+     *
+     * @param key
+     * @param field
+     * @param value
+     * @return
+     */
+    public static long hset(String key, String field, String value) {
+
+        try (Jedis jedis = getJedis()) {
+            if (jedis.exists(key)) {
+                //设置内容
+                return jedis.hset(key, field, value);
+            } else {
+                Long res = jedis.hset(key, field, value);
+                //设置过期时间:(时间单位:秒),2880*60=172800
+                jedis.expire(key, Integer.parseInt(SysParamUtil.getParam("ExpiredTime")));
+                return res;
+            }
+        }
+    }
+
+    /**
+     * 删除redis哈希内容
+     *
+     * @param key
+     * @return
+     */
+    public static long hdel(String key) {
+
+        try (Jedis jedis = getJedis()) {
+            Long res = jedis.del(key);
+            if (res == 0L) {
+                logger.error("hdel result is 0,key:[{}]", key);
+            }
+            return res;
+        }
+    }
 
 	/**
-	  * 获取redis哈希内容
-	  * @param key
-	  * @param field
-	  * @return
-	  */
-	public static String hget(String key,String field){
+	 * 获取redis哈希内容
+	 *
+	 * @param key
+	 * @param field
+	 * @return
+	 */
+	public static String hget(String key, String field) {
 
-	 	 String value = null;
-		 Jedis jedis =null;
-		 try {
-		        jedis = getJedis();
-	            value = jedis.hget(key, field);
-	        } finally {
-	            //返还到连接池
-	            returnResource(jedis);
-	        }
-	        return value;
-	    }
-	 
-	 /**
-	  * 设置redis哈希内容, 对哈希进行hset不会修改已经设置的key过期时间
-	  * @param key
-	  * @param field
-	  * @param value
-	  * @return
-	  */
-	 @SuppressWarnings("deprecation")
-	public static Long hset(String key,String field,String value){
-	        Long res = 0L;
-	        
-	        Jedis jedis = null;
-	        try {
-		        jedis = getJedis();
-	            if (jedis.exists(key)) {					
-	            	//设置内容
-	            	res = jedis.hset(key, field, value);
-				}else{
-					res = jedis.hset(key, field, value);
-					//设置过期时间:(时间单位:秒),2880*60=172800
-					jedis.expire(key, Integer.parseInt(SysParamUtil.getParam("ExpiredTime")));
-				}
-	        } catch (Exception e) {
-	            //释放redis对象
-	            logger.error("error",e);
-	        } finally {
-	            //返还到连接池
-	            returnResource(jedis);
-	        }
-	        
-	        return res;
-	    }
-	 
-	 /**
-	  * 删除redis哈希内容
-	  * @param key
-	  * @return
-	  */
-	 @SuppressWarnings("deprecation")
-	public static Long hdel(String key){
-	        Long res = 0L;
-	        
-	        Jedis jedis = null;
-	        try {
-		        jedis = getJedis();
-		        res = jedis.del(key);
-	        } catch (Exception e) {
-	            //释放redis对象
-	            logger.error("error",e);
-	        } finally {
-	            //返还到连接池
-	            returnResource(jedis);
-	        }
-	        return res;
-	    }
-	 
-	 	/**
-	 	 *  根据key获取redis的内容
-	 	 * @param key
-	 	 * @return
-	 	 */
-		@SuppressWarnings("deprecation")
-		public static String get(String key) {
-			String res = null;
-
-	        Jedis jedis = null;
-			try {
-				jedis = getJedis();
-				res = jedis.get(key);
-			} catch (Exception e) {
-				// 释放redis对象
-				logger.error("error",e);
-			} finally {
-				// 返还到连接池
-				returnResource(jedis);
-			}
-			return res;
+		try (Jedis jedis = getJedis()) {
+			return jedis.hget(key, field);
 		}
-		
-		/**
-		 * 判断key是否存在
-		 * @param key
-		 * @return
-		 */
-		@SuppressWarnings("deprecation")
-		public static Boolean exists(String key) {
-			Boolean res = null;
-
-	        Jedis jedis = null;
-			try {
-				jedis = getJedis();
-				res = jedis.exists(key);
-			} catch (Exception e) {
-				// 释放redis对象
-				logger.error("error",e);
-			} finally {
-				// 返还到连接池
-				returnResource(jedis);
-			}
-			return res;
-		}
+	}
 }
