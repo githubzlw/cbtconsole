82c4b155fdce9fe29fdf5e764c42d8342d08c75d
Jiangxw <1071083166@qq.com>
2019-12-17 09:52:53 +0800

    IPN取消对比本地详情数据

--- src/main/java/com/cbt/report/vo/OrderCancelBean.java
+++ src/main/java/com/cbt/report/vo/OrderCancelBean.java
@@ -1,85 +1,27 @@
 package com.cbt.report.vo;
 
+import lombok.Data;
+
 /**
  * 订单取消(全部或部分)详情
  *
  * @author JXW
  */
+@Data
 public class OrderCancelBean {
 
     private String year;// 年份
     private String month;// 月份
     private int userId;// 客户id
+
+    private String ipnOrderNo;
+    private String txnId;
+    private double payAmount;
+    private String payTime;
+
     private double amount;// 取消商品金额
     private String remarkId;//备注ID一般是订单号
     private String remark;//备注信息
     private int payType = -1;// 退款类型
 
-    public int getPayType() {
-        return payType;
-    }
-
-    public void setPayType(int payType) {
-        this.payType = payType;
-    }
-
-    public String getYear() {
-        return year;
-    }
-
-    public void setYear(String year) {
-        this.year = year;
-    }
-
-    public String getMonth() {
-        return month;
-    }
-
-    public void setMonth(String month) {
-        this.month = month;
-    }
-
-    public int getUserId() {
-        return userId;
-    }
-
-    public void setUserId(int userId) {
-        this.userId = userId;
-    }
-
-    public double getAmount() {
-        return amount;
-    }
-
-    public void setAmount(double amount) {
-        this.amount = amount;
-    }
-
-    public String getRemarkId() {
-        return remarkId;
-    }
-
-    public void setRemarkId(String remarkId) {
-        this.remarkId = remarkId;
-    }
-
-    public String getRemark() {
-        return remark;
-    }
-
-    public void setRemark(String remark) {
-        this.remark = remark;
-    }
-
-    @Override
-    public String toString() {
-        return "OrderCancelBean{" +
-                "year='" + year + '\'' +
-                ", month='" + month + '\'' +
-                ", userId=" + userId +
-                ", amount=" + amount +
-                ", remarkId='" + remarkId + '\'' +
-                ", remark='" + remark + '\'' +
-                '}';
-    }
 }
