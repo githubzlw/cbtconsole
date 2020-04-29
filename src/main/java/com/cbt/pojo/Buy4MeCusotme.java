81e86521daa78484a07030bcb73f88025dcf9b2d
cjc <saycjc@outlook.com>
2020-04-29 15:50:02 +0800

    按照需求从新修改购物车营销

--- src/main/java/com/cbt/pojo/Buy4MeCusotme.java
+++ src/main/java/com/cbt/pojo/Buy4MeCusotme.java
@@ -1,6 +1,11 @@
 package com.cbt.pojo;
 
+import com.importExpress.pojo.Address;
+import com.importExpress.pojo.UserBean;
 import lombok.Data;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * *****************************************************************************************
@@ -23,4 +28,20 @@ public class Buy4MeCusotme {
     public String jumpLink;
     public boolean hasMsg;
     public String adm;
+
+    private String sessionId;
+    private Double totalPrice = 0D;
+    private Double goodsCost = 0D;
+    private Integer totalNum = 0;
+    private Integer itemNum = 0;
+    private Double itemPrice = 0D;
+    private Double serviceFee = 0D;
+    private String orderNo;
+    private int state = -2;
+    private String our_order_no;
+    private String deliveryTime;
+    private String deliveryMethod;
+    private String shipFeight;
+    private int sampleFlag = 0;
+    private String country = "Tourist";
 }
